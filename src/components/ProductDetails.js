5b37e18a024c08734fea0e0ec4898a2db86139e7
mauro404 <mau.mansano@gmail.com>
2022-10-29 13:50:30 +0200

    create add to watchlist functionality

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -5,23 +5,23 @@ import { useState } from "react";
 
 const API_URL = "http://localhost:5006";
 
-function ProductDetails({products}) {
-    const { id } = useParams();
-    const [amount, setAmount] = useState(1)
+function ProductDetails({ products }) {
+  const { id } = useParams();
+  const [amount, setAmount] = useState(1);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const details = products.find(product => product._id === id)
+  const details = products.find((product) => product._id === id);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newOrder = {
-            items: {
-                product: details,
-                amount
-            }
-        }
-        const storedToken = localStorage.getItem("authToken");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newOrder = {
+      items: {
+        product: details,
+        amount,
+      },
+    };
+    const storedToken = localStorage.getItem("authToken");
 
     axios
       .post(`${API_URL}/api/orders/create`, newOrder, {
@@ -33,27 +33,53 @@ function ProductDetails({products}) {
       })
       .catch((e) => console.log("error creating order on API", e));
     // //clear form
-  }
+  };
 
-    return(
-        <div>
-            <img src={details.image_URL} alt={details.title}/>
-            <h2>{details.title}</h2>
-            <p>{details.description}</p>
-            <p>{details.price}</p>
-            <p>{details.specs}</p>
-            <p>{details.rating}</p>
-            <Link to= {`/products/${details._id}/edit`}>Edit this product</Link>
-            <form onSubmit={handleSubmit}>
-            <input type="hidden" value={details}/>
-            <label>Amount</label>
-            <input type="number" min={1} value={amount} onChange={(e) => setAmount(e.target.value)}/>
+  const handleSubmitWatchlist = (e) => {
+    e.preventDefault();
+    const newProduct = {
+      newItem: details,
+    }
+    const storedToken = localStorage.getItem("authToken");
 
-                <button type="submit">Buy Now</button>
-            </form>
-        </div>
-    )
+    axios
+      .post(`${API_URL}/api/products/${id}/add`, newProduct, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      // .then((response) => {
+      //   //fetchProductsCB();
+      //   navigate("/");
+      // })
+      .catch((e) => console.log("error adding to watchlist", e));
+    // //clear form
+  };
+  return (
+    <div>
+      <img src={details.image_URL} alt={details.title} />
+      <h2>{details.title}</h2>
+      <p>{details.description}</p>
+      <p>{details.price}</p>
+      <p>{details.specs}</p>
+      <p>{details.rating}</p>
+      <Link to={`/products/${details._id}/edit`}>Edit this product</Link>
+      <form onSubmit={handleSubmit}>
+        <input type="hidden" value={details} />
+        <label>Amount</label>
+        <input
+          type="number"
+          min={1}
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+
+        <button type="submit">Buy Now</button>
+      </form>
+      <form onSubmit={handleSubmitWatchlist}>
+        <input type="hidden" value={details} />
+        <button type="submit">Add to watchlist</button>
+      </form>
+    </div>
+  );
 }
-
 
 export default ProductDetails;
